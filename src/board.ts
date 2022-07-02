5d083aa577a4b9526781c7f124a5d91437607cba
Youssef <cefyou2002@gmail.com>
2022-07-02 16:36:49 +0100

    Added key and random

--- src/board.ts
+++ src/board.ts
@@ -1,31 +1,65 @@
 import { Tile } from "./tile";
 
+export enum Direction {
+  Up,
+  Down,
+  Right,
+  Left,
+}
+
 export class Board {
 
-    private board: Tile[] = [];
+  private board: Tile[] = [];
 
-    public Start() {
-        console.log('Starting 2048');
-        this.board = [
-            { x: 1, y: 1, val: 2 },
-            { x: 2, y: 1, val: 4 },
-            { x: 3, y: 1, val: 8 },
-            { x: 4, y: 1, val: 16 },
-            { x: 1, y: 2, val: 32 },
-            { x: 2, y: 2, val: 64 },
-            { x: 4, y: 4, val: 2048 },
-        ]
+  public Start() {
+    console.log('Starting 2048');
+    this.board = [
+      { x: 1, y: 1, val: 2 },
+      { x: 2, y: 1, val: 4 },
+      { x: 3, y: 1, val: 8 },
+      { x: 4, y: 1, val: 16 },
+      { x: 1, y: 2, val: 32 },
+      { x: 2, y: 2, val: 64 },
+      { x: 4, y: 1, val: 64 },
+      { x: 3, y: 3, val: 64 },
+      { x: 4, y: 4, val: 2048 },
+
+    ]
+    this.AddTile();
+    this.Draw();
+  }
+
+  public Move(direction: Direction) {
+    this.AddTile();
+    this.Draw();
+  }
+
+  private AddTile() {
+    const empty: Tile[] = [];
+
+    for (let x = 1; x <= 4; x++) {
+      for (let y = 1; y <= 4; y++) {
+        if (!this.board.find(tile => tile.x === x && tile.y === y)) empty.push({ x, y, val: 2 });
+      }
     }
+    if (!empty.length) return this.Lose();
+    const index = Math.floor(Math.random() * empty.length);
+    this.board.push(empty[index])
+  }
 
-    public Draw() {
-        for (const tile of this.board) {
-            const tiles = document.querySelectorAll('.tile') as any;
-            tiles?.forEach((t: any) => {
-                if (t.dataset.col === String(tile.x) && t.dataset.row === String(tile.y)) {
-                    t.classList.add('active', `val_${tile.val}`);
-                    t.innerHTML = tile.val;
-                } 
-            })
+  public Lose() {
+    console.log('You lose')
+  }
+
+  public Draw() {
+    for (const tile of this.board) {
+      const tiles = document.querySelectorAll('.tile') as any;
+      tiles?.forEach((t: any) => {
+        if (t.dataset.col === String(tile.x) && t.dataset.row === String(tile.y)) {
+          t.classList.add('active', `val_${tile.val}`);
+          t.innerHTML = tile.val;
         }
+      })
     }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import { Board } from './board'
+import { Board, Direction } from './board'
 import './scss/style.scss'
 
 const game = new Board();
 game.Start();
-game.Draw();
+
+
+const onKeyPress = (evt: any) => {
+  switch (evt.key) {
+    case 'ArrowUp': game.Move(Direction.Up); break;
+    case 'ArrowDown': game.Move(Direction.Down); break;
+    case 'ArrowLeft': game.Move(Direction.Left); break;
+    case 'ArrowRight': game.Move(Direction.Right); break;
+  }
+}
+
+document.addEventListener('keyup', onKeyPress);
